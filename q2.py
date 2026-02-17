ed589cb88af0479dcd6837db1eed412089123e52
Charanteja1825 <suryacherry0@gmail.com>
2026-02-17 20:56:34 +0530

    Time Converter

--- q2.py
+++ q2.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     print(convert_seconds(45))
     print(convert_seconds(3600))
     print(convert_seconds(0))
-
+    
     assert convert_seconds(125) == "2m 5s"
     assert convert_seconds(60) == "1m 0s"
     assert convert_seconds(45) == "0m 45s"
